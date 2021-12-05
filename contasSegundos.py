08392111d9a15276dfd8a1d6166dd769d74fe8fd
Gustavo Nascimento <o_fantasma@hotmail.com.br>
2021-12-05 18:45:32 -0300

    Update contasSegundos.py

--- contasSegundos.py
+++ contasSegundos.py
@@ -6,4 +6,4 @@ segs_restantes = total_segs % 3600
 minutos = segs_restantes // 60
 segs_restantes_final = segs_restantes % 60
 
-print(horas, "horas, ", minutos, "minutos e", segs_restantes_final, "segundos. ")
+print(horas, "horas ", minutos, "minutos e", segs_restantes_final, "segundos. ")
